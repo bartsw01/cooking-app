2a331d9fab41945dd2957cd2faaccccb22e0774a
Bart Webber and Paul Guevarra <bart@>
2017-07-03 13:47:18 -0700

    add cooking.html with styles.css and scripts.js.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var c = function(myCelsius){
+	return myCelsius * (9 / 5) + 32};
+
+
+var myCelsius = parseInt(prompt("What is the temperature in Celsius?"));
+
+
+alert("The Temp in F is " + c(myCelsius) + ".");
